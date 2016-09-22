9cab793acc78f751ff48f583e5ba437a7cbfa25d
Pat Gaffney <pat@hypepat.com>
2016-09-22 01:47:49 -0500

    ADD: Update the publishing settings

--- publishconf.py
+++ publishconf.py
@@ -8,17 +8,27 @@ from __future__ import unicode_literals
 import os
 import sys
 sys.path.append(os.curdir)
+
+# Import settings from pelicanconf.py file.
 from pelicanconf import *
 
 SITEURL = 'http://hypepat.com'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ATOM = 'feeds/atom.xml'
+FEED_RSS = None
+FEED_MAX_ITEMS = 10
+FEED_ALL_RSS = None
+FEED_ALL_ATOM = None
+TAG_FEED_RSS = None
+TAG_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+AUTHOR_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
+CATEGORY_FEED_ATOM = None
+
+# output to public_html/
+# this is set in Makefile
+# OUTPUT_PATH = 'public_html'
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
