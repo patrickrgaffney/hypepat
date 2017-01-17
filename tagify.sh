0eaf948a507a3eb935f9306b37795dfbe28565b3
Pat Gaffney <pat@hypepat.com>
2017-01-16 21:48:36 -0600

    Add documentation to tagify.sh

--- tagify.sh
+++ tagify.sh
@@ -5,31 +5,92 @@
 #  created:    2017-01-16
 #  modified:   2017-01-16
 #  project:    hypepat.com
+#  license:    MIT
+#
+# This script searches a Jekyll repository for all of your posts, parses
+# the tags/topics/categories/whatever (henceforth referred to as tags) out 
+# of their YAML front matter, and creates an HTML file for each unique tag
+# in a directory of your choosing.
+#
+# This script is fairly configurable -- see the USER CONFIGURATION section
+# below, but it does assume that you use YAML's bracketed sequence to 
+# denote your tags:
+#
+#       ---
+#       ...
+#       tag: [git, bash, zsh]
+#       ...
+#       ---
+#
+# It would be trivial to change this to support other YAML sequence 
+# types: list-syntax, scalars-to-scalars, etc.
+#
+# In order for this script to work properly, you need to create a 
+# Jekyll collection in your _config.yml file as such:
+#
+#       collections:
+#           topics:
+#               output: true
+#               permalink: /topics/:path.html
+#
+# This tells Jekyll to search in the _topics/ directory for HTML pages
+# that will need rendering at build-time. This can be added to your
+# build script to ensure that everytime you write a new post with a new
+# tag, that file is automatically created. This output directory is
+# configurable via a global constant in the USER CONFIGURATION section.
+#
+# The actual text that is written to the created HTML file is at the 
+# bottom of this file in a bash HERE document. You can change it to be
+# whatever you would like.
 #
 #########################################################################
 
 #!/usr/bin/env bash
 
-## USER CONFIGURATION ##
+#########################################################################
+# USER CONFIGURATION 
+#
+# These global constants control where the input comes from, how the
+# parsing is done, and where the output is written. They can be changed
+# on a project-basis to better reflect your needs.
+#
+# - POSTDIRS: the directory to search for posts, relative to $CWD
+# - TAGSPREFIX: the YAML scalar variable that you want to parse
+# - TAGSDIR: the directory you want the generated HTML files written to
+# - TAGSDIVISOR: a replacement for whitespace in multi-word tags
+#
+#########################################################################
 POSTSDIR="$PWD/_posts"
 TAGSPREFIX="tags:"
 TAGSDIR="$PWD/_topics"
 TAGDIVISOR="-"
 
-## Colors ##
+#########################################################################
+# ANSI COLOR SUPPORT
+#
+# If your terminal does not support ANSI colors, assign each of these
+# constants an empty string, or enable colors.
+#
+#########################################################################
 GREEN=$'\e[32m'
 BOLD=$'\e[1m'
 RED=$'\e[31m'
 RESET=$'\e[0m'
 
-## TOTALS ##
 TAGFILES=0
 
+#########################################################################
+# CHECK THE VERSION OF BASH
+#
+# This script makes extensive use of bash arrays, which we're introduced
+# in bash 4.0. You will need to upgrade if your still using bash 3.x.
+#
+#########################################################################
 function check_version {
     local bashversion=${BASH_VERSION:0:1}
     
     if [[ $bashversion != "4" ]]; then
-        echo -e "$BOLD$RED ERROR:$RESET this script requires bash >= 4.0"
+        echo -e "$BOLD$RED ERROR:$RESET$BOLD this script requires bash >= 4.0$RESET"
         exit 1
     else
         echo -e "$BOLD --> searching for tags...$RESET"
@@ -37,6 +98,14 @@ function check_version {
     fi
 }
 
+
+#########################################################################
+# PARSE THE POSTS FILES FOR TAGS
+#
+# Each of the files in $POSTSDIR is parsed looking for tags. As the tags
+# are found, they are printed to stdout and saved to an indexed array.
+#
+#########################################################################
 function read_tags {
     declare -a tags
     local tagline=''
@@ -76,6 +145,15 @@ function read_tags {
     remove_duplicates tags[@]
 }
 
+
+#########################################################################
+# REMOVE ANY DUPLICATE TAGS FROM THE ARRAY
+#
+# Because the tags are added to the array as they are parsed, there are
+# probably duplicates in the array. The array is looped through once, 
+# and all duplicates are pruned as they are found.
+#
+#########################################################################
 function remove_duplicates {
     declare -a oldarray=("${!1}")
     declare -A seen
@@ -95,13 +173,24 @@ function remove_duplicates {
     create_tag_files ${tags[@]}
 }
 
+
+#########################################################################
+# DETERMINE WHICH TAGS NEED FILES
+#
+# Everytime this script is run, it may or may not create any new files.
+# If all of the tags found when parsing already have files, then no new
+# files need to be created.
+#
+#########################################################################
 function create_tag_files {
     declare -a newtags=("${@}")
     local tagline=''
     
     for tag in "${tags[@]}"
     do
+        # Replace spaces in tag name with $TAGDIVISOR.
         tagline=${tag//[[:space:]]/"$TAGDIVISOR"}
+        
         if [ -f "$TAGSDIR/$tagline.html" ]; then
             printf "$BOLD --> skipping $tag: html file already exists$RESET\n"
         else
@@ -113,14 +202,22 @@ function create_tag_files {
     printf "$BOLD --> created $TAGFILES new tag files!$RESET\n"
 }
 
+
+#########################################################################
+# CREATE AND WRITE TO A NEW TAG FILE
+#
+# This function is called once for each tag that needs a file. The tag
+# name itself (with spaces) is available as the first parameter: $1. The
+# enter HERE document is written to the file, so it can be changed to 
+# suit your needs.
+#
+#########################################################################
 function write_tag_file {
-# -----------------------------------------------------------
-# 'Here document containing the body of the generated script.
 (
 cat <<EOF
 ---
 title: $1
-layout: paged
+layout: page
 ---
 
 {% assign site_tags = site.tags | sort | compact %}
@@ -144,7 +241,10 @@ layout: paged
 
 EOF
 ) > "$TAGSDIR/$1.html"
-# -----------------------------------------------------------
 }
 
+
+#########################################################################
+# BEGIN EXECUTION
+#########################################################################
 check_version
